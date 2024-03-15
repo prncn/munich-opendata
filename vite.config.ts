48f949fbe4205cb58b7e63b5050b39d3633545a1
prncn <princen@outlook.de>
2024-03-15 12:56:31 +0100

    Change vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/munich-opendata/',
   server: {
     port: 8000,
-    proxy: {
-      '*': {
-        target: 'https://opendata.muenchen.de',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-      },
-    },
   }
 })
